e5c8dca541e567ad328ba853984c490c2283beda
felnaf <119995992+felnaf@users.noreply.github.com>
2023-01-25 09:51:32 +0530

    Add files via upload

---
+++ Blog/src/service.js
@@ -0,0 +1,16 @@
+import instance from './api/instance';
+import cryptoJs from 'crypto-js';
+
+export const getData = (url) => instance.get(url);
+export const postData = (url, data) => instance.post(url, data);
+export const deleteData = (url) => instance.delete(url);
+export const editData = (url, data) => instance.patch(url, data);
+
+export const validatePassword = (ciphertext, password) => {
+  var bytes = cryptoJs.AES.decrypt(ciphertext, 'my-secret-key@123');
+  var decrypedPassword = JSON.parse(bytes.toString(cryptoJs.enc.Utf8));
+  if (decrypedPassword === password) {
+    return true;
+  }
+  return false;
+};
